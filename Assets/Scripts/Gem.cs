b7225b82bb8adcd220f8586e0be7311a46eda514
Venki <18534venkanna@gmail.com>
2025-01-25 00:47:35 +0530

    Swiping Gems

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Gem : MonoBehaviour
@@ -8,10 +9,38 @@ public class Gem : MonoBehaviour
     [HideInInspector]
     public Board board;
     
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    
+    private bool mousePressed;
+    private float swipeAngle = 0f;
 
+    private void Update()
+    {
+        if (mousePressed && Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+            firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+        }
+    }
+    
     public void SetupGem(Vector2Int pos, Board theboard)
     {
         posIndex = pos;
         board = theboard;
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePressed = true;
+    }
+
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
+        swipeAngle = swipeAngle * Mathf.Rad2Deg;
+        Debug.Log(swipeAngle);
     }
 }
