3377cd536ca5be6775cb4cd6b807bc0a2f28752e
Venki <18534venkanna@gmail.com>
2025-01-25 11:55:31 +0530

    Finding Matches

---
+++ Assets/Scripts/MatchFinder.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class MatchFinder : MonoBehaviour
+{
+    private Board board;
+
+    private void Awake()
+    {
+        board = FindObjectOfType<Board>();
+    }
+
+    public void FindAllMatches()
+    {
+        for (int x = 0; x < board.width; x++)
+        {
+            for (int y = 0; y < board.height; y++)
+            {
+                Gem currentGem = board.allGems[x, y];
+                if (currentGem != null)
+                {
+                    if (x > 0 && x < board.width - 1)
+                    {
+                        Gem leftGem = board.allGems[x - 1, y];
+                        Gem rightGem = board.allGems[x + 1, y];
+                        if (leftGem != null && rightGem != null)
+                        {
+                             
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
